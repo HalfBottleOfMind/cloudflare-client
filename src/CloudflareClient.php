e72a65bce0ca71618d7f77b48cba0ccceac4a992
Vladislav Panfilov <andromalak222@gmail.com>
2020-10-29 16:50:47 +0500

    packages

---
+++ src/CloudflareClient.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace HalfBottleOfMind\CloudflareClient;
+
+class CloudflareClient
+{
+    private const BASE_URL = 'https://api.cloudflare.com/client/v4/';
+}
---
+++ tests/Unit/Tests.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Client;
+
+use HalfBottleOfMind\CloudflareClient\CloudflareClient;
+use Tests\TestCase;
+
+class Tests extends TestCase
+{
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function testBaseTest(): void
+    {
+        $this->assertTrue(true);
+    }
+}
